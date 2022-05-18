f96d5cc06ddca45eafe146157d74a5bf4832c4a9
DanteFrank <davidfrankoziwo@gmail.com>
2022-05-18 15:30:18 +0100

    Urls

--- index.js
+++ index.js
@@ -2,12 +2,34 @@ const http = require("http");
 
 const PORT = 3000;
 
+const friends = [
+  {
+    id: 0,
+    name: "John",
+  },
+  {
+    id: 1,
+    name: "Jane",
+  },
+  {
+    id: 2,
+    name: "Joe",
+  },
+];
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/friends") {
+  const items = req.url.split("/");
+
+  // /friends/{id} => ['', 'friends', '{id}']
+  if (items[1] === "friends") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ id: 1, name: "John" }));
-  } else if (req.url === "/messages") {
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === "messages") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(
       "<html><body><ul><li>Message 1</li><li>Message 2</li></ul></body></html>"
