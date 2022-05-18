284442b6405a8cf051d7d6ccc747fdd43a086675
DanteFrank <davidfrankoziwo@gmail.com>
2022-05-18 10:59:53 +0100

    HTTP APIs and Routing

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const http = require("http");
 
 const PORT = 3000;
 
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Tyep": "text/plain" });
-  res.end(JSON.stringify({ id: 1, name: "John" }));
+  if (req.url === "/friends") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ id: 1, name: "John" }));
+  } else if (req.url === "/messages") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(
+      "<html><body><ul><li>Message 1</li><li>Message 2</li></ul></body></html>"
+    );
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
